51da3b7e8817dccc3fd721ba57f6650aa73c8720
Ummaromana Sama <ummaromanasama@gmail.com>
2020-05-03 23:03:07 -0400

    Add files via upload
    connect the dots of the image (random)

--- portrait.py
+++ portrait.py
@@ -1,28 +1,23 @@
+#Importing Libraries
 from PIL import Image  
-
-original_image = Image.open("jg.jpg") # open colour image
-bw_image = original_image.convert('1') # convert image to black and white
-#image_file.save('result.png')
-# bw_image.show()
-# original_image.show()
-  
 import numpy as np  
 import matplotlib.pyplot as plt  
-  
+
+#Load image and conver to black and white
+original_image = Image.open("jg.jpg") 
+bw_image = original_image.convert('1') 
 bw_image_array = np.array(bw_image, dtype=np.int)  
 black_indices = np.argwhere(bw_image_array == 0)  
-# Changing "size" to a larger value makes this algorithm take longer,  
-# but provides more granularity to the portrait  
-chosen_black_indices = black_indices[  
-                           np.random.choice(black_indices.shape[0],  
-                                            replace=False,  
-                                            size=90000)]  
-  
-plt.figure(figsize=(6, 8), dpi=100)  
-plt.scatter([x[1] for x in chosen_black_indices],  
-            [x[0] for x in chosen_black_indices],  
-            color='black', s=1)  
+chosen_black_indices = black_indices[np.random.choice(black_indices.shape[0], replace=False, size=1000)]  
+
+#Black and white image popup 
+plt.figure(figsize=(5, 5), dpi=100)  
+plt.scatter([x[1] for x in chosen_black_indices], [x[0] for x in chosen_black_indices], color='black', s=1)  
 plt.gca().invert_yaxis()  
 plt.xticks([])  
-plt.yticks([]) 
+plt.yticks([])
+plt.plot([x[1] for x in chosen_black_indices], [x[0] for x in chosen_black_indices], marker='o', color='black') 
 plt.show()
+
+
+
